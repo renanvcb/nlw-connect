a333bb9e9bf7013a7cab59d02899ac304687a3ee
Renan Borges <renanvcb@gmail.com>
2025-02-18 20:22:41 -0300

    feat: major linting with biomejs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,37 @@
+import { ArrowRight, Copy, User } from 'lucide-react'
+import { Button } from './components/button'
+import { IconButton } from './components/icon-button'
+import { InputField, InputIcon, InputRoot } from './components/input'
+
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <Button>
+        Enviar
+        <ArrowRight />
+      </Button>
+
+      <IconButton>
+        <Copy />
+      </IconButton>
+
+      <div>
+        <InputRoot>
+          <InputIcon>
+            <User className="size-5" />
+          </InputIcon>
+
+          <InputField placeholder="Placeholder" />
+        </InputRoot>
+
+        <InputRoot error>
+          <InputIcon>
+            <User className="size-5" />
+          </InputIcon>
+
+          <InputField placeholder="Placeholder" />
+        </InputRoot>
+      </div>
     </main>
-  );
+  )
 }
