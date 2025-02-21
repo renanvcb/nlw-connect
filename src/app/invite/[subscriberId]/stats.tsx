23216de5e64f832789d7733c77f4c939e1ebd71d
Renan Borges <renanvcb@gmail.com>
2025-02-21 19:38:33 -0300

    feat: invite card component removed

--- src/app/invite/[subscriberId]/stats.tsx
+++ src/app/invite/[subscriberId]/stats.tsx
@@ -1,24 +1,52 @@
+import {
+  getSubscriberInviteClicks,
+  getSubscriberInviteCount,
+  getSubscriberRankingPosition,
+} from '@/http/api'
 import { BadgeCheck, Medal, MousePointerClick } from 'lucide-react'
-import { InviteCard } from './invite-card'
 
-export function Stats() {
+interface StatsProps {
+  subscriberId: string
+}
+
+export async function Stats({ subscriberId }: StatsProps) {
+  const { count: accessCount } = await getSubscriberInviteClicks(subscriberId)
+  const { count: inviteCount } = await getSubscriberInviteCount(subscriberId)
+  const { position: rankingPosition } =
+    await getSubscriberRankingPosition(subscriberId)
+
   return (
     <div className="grid gap-3 md:grid-cols-3">
-      <InviteCard
-        dataText="1042"
-        title="Acessos ao link"
-        icon={<MousePointerClick className="size-5" />}
-      />
-      <InviteCard
-        dataText="700"
-        title="Inscrições feitas"
-        icon={<BadgeCheck className="size-5" />}
-      />
-      <InviteCard
-        dataText="3º"
-        title="Posição no ranking"
-        icon={<Medal className="size-5" />}
-      />
+      <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
+        <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+          {accessCount}
+        </span>
+        <span className="text-sm text-gray-300 leading-none text-center">
+          Acessos ao link
+        </span>
+
+        <MousePointerClick className="size-5 text-purple absolute top-3 left-3" />
+      </div>
+      <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
+        <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+          {inviteCount}
+        </span>
+        <span className="text-sm text-gray-300 leading-none text-center">
+          Inscrições feitas
+        </span>
+
+        <BadgeCheck className="size-5 text-purple absolute top-3 left-3" />
+      </div>
+      <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
+        <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+          {rankingPosition ? `${rankingPosition}º` : '-'}
+        </span>
+        <span className="text-sm text-gray-300 leading-none text-center">
+          Posição no ranking
+        </span>
+
+        <Medal className="size-5 text-purple absolute top-3 left-3" />
+      </div>
     </div>
   )
 }
