fa686ae14e3b5d9005844813d8373cae7645f71d
Renan Borges <renanvcb@gmail.com>
2025-02-20 21:36:46 -0300

    feat: moved home page to its own directory

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,8 @@
 import Image from 'next/image'
 
 import logo from '@/assets/logo.svg'
-import { ArrowRight, Mail, Radio, User } from 'lucide-react'
-import { Button } from '../components/button'
-import { InputField, InputIcon, InputRoot } from '../components/input'
+import { Radio } from 'lucide-react'
+import { SubscriptionForm } from './subscription-form'
 
 export default function Home() {
   return (
@@ -36,34 +35,8 @@ export default function Home() {
             <br /> Dias 15 a 17 de março | Das 18h às 21h | Online & Gratuito
           </p>
         </div>
-        <form className="bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 w-full md:max-w-[440px]">
-          <h2 className="font-heading font-semibold text-gray-200 text-xl">
-            Inscrição
-          </h2>
 
-          <div className="space-y-3">
-            <InputRoot>
-              <InputIcon>
-                <User />
-              </InputIcon>
-
-              <InputField type="text" placeholder="Nome completo" />
-            </InputRoot>
-
-            <InputRoot>
-              <InputIcon>
-                <Mail />
-              </InputIcon>
-
-              <InputField type="email" placeholder="E-mail" />
-            </InputRoot>
-          </div>
-
-          <Button type="submit">
-            Confirmar
-            <ArrowRight className="size-5" />
-          </Button>
-        </form>
+        <SubscriptionForm />
       </div>
     </div>
   )
