10d135481296e9780cb52d92c5d23a1a4fa7389d
Renan Borges <renanvcb@gmail.com>
2025-02-21 19:11:54 -0300

    feat: subscription form integrated with api to new user

--- src/app/(home)/subscription-form.tsx
+++ src/app/(home)/subscription-form.tsx
@@ -2,8 +2,10 @@
 
 import { Button } from '@/components/button'
 import { InputField, InputIcon, InputRoot } from '@/components/input'
+import { subscribeToEvent } from '@/http/api'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { ArrowRight, Mail, User } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 
@@ -15,6 +17,8 @@ const subscriptionSchema = z.object({
 type SubscriptionSchema = z.infer<typeof subscriptionSchema>
 
 export function SubscriptionForm() {
+  const router = useRouter()
+
   const {
     register,
     handleSubmit,
@@ -25,8 +29,10 @@ export function SubscriptionForm() {
     reValidateMode: 'onSubmit',
   })
 
-  function onSubscribe(data: SubscriptionSchema) {
-    console.log(data)
+  async function onSubscribe({ name, email }: SubscriptionSchema) {
+    const { subscriberId } = await subscribeToEvent({ name, email })
+
+    router.push(`/invite/${subscriberId}`)
   }
 
   return (
